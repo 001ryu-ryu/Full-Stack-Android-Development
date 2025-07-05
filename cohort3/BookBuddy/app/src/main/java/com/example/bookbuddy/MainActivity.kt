7fd42e1f58f62554c5db2ddde2f3117666bb2456
Iftikar Hussain <iftikarraza8@gmail.com>
2025-07-05 19:29:51 +0530

    initiated E Book Project using firebase

---
+++ cohort3/BookBuddy/app/src/main/java/com/example/bookbuddy/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.bookbuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookbuddy.ui.theme.BookBuddyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BookBuddyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
