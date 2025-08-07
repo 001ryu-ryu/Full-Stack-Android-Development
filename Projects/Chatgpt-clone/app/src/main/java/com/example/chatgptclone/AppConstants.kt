a33bcd7a5565105ed7879c1811ced60284e0be89
Iftikar Hussain <iftikarraza8@gmail.com>
2025-08-07 16:11:25 +0530

    ugh

--- Projects/Chatgpt-clone/app/src/main/java/com/example/chatgptclone/AppConstants.kt
+++ Projects/Chatgpt-clone/app/src/main/java/com/example/chatgptclone/AppConstants.kt
@@ -2,4 +2,4 @@ package com.example.chatgptclone
 
 const val BASE_URL = "https://openrouter.ai/api/v1/"
 const val CHAT_GPT_MODEL = "deepseek/deepseek-r1:free"
-const val API_KEY = "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"
+const val API_KEY = "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"
