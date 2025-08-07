db0fc8fe14fce84a02f75124f3184a56584cc688
Iftikar Hussain <iftikarraza8@gmail.com>
2025-08-08 00:05:49 +0530

    Did some experiment on ai

--- Projects/Chatgpt-clone/app/src/main/java/com/example/chatgptclone/AppConstants.kt
+++ Projects/Chatgpt-clone/app/src/main/java/com/example/chatgptclone/AppConstants.kt
@@ -1,5 +1,5 @@
 package com.example.chatgptclone
 
 const val BASE_URL = "https://openrouter.ai/api/v1/"
-const val CHAT_GPT_MODEL = "deepseek/deepseek-r1:free"
-const val API_KEY = "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"
+const val CHAT_GPT_MODEL = "z-ai/glm-4.5-air:free"
+const val API_KEY = ""
